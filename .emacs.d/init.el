40538476dee2416c1e0ff3accc4276e2a2b5ec8c
Glutexo <glutexo@icloud.com>
2022-07-11 19:29:17 +0200

    Add Emacs custom configuration
    Autogenerated configuration lines that appeared after installing Magit.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,3 +2,15 @@
 (setq inhibit-splash-screen t)
 (tool-bar-mode -1)
 (global-display-line-numbers-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(markdown-mode magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
